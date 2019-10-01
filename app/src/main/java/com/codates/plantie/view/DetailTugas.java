dbe679e9d0fdb126c3742a385b78761a4d7a243d
Iman Nurohman <imannurohman1@gmail.com>
2019-10-01 12:29:42 +0700

    DetailTugas

---
+++ app/src/main/java/com/codates/plantie/view/DetailTugas.java
@@ -0,0 +1,21 @@
+package com.codates.plantie.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.codates.plantie.R;
+
+public class DetailTugas extends AppCompatActivity {
+    public static  final String EXTRA_POSISITION = "extra_posisition";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_tugas);
+        int position = getIntent().getIntExtra(EXTRA_POSISITION,0);
+
+
+    }
+}

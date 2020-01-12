2c292263839661e4322d0719d0c10752521a33c2
Iqbal_Mubarok <muhammadiqbalmubarok@gmail.com>
2020-01-12 09:59:39 +0700

    update laporan

---
+++ app/src/main/java/com/codates/plantie/view/Laporan.java
@@ -0,0 +1,44 @@
+package com.codates.plantie.view;
+
+import android.os.Bundle;
+
+import com.codates.plantie.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.codates.plantie.view.ui.main.SectionsPagerAdapter;
+
+public class Laporan extends AppCompatActivity {
+
+    public static final String EXTRA_POSITION = "extra_position";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_laporan);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        tabs.setTabMode(TabLayout.MODE_SCROLLABLE);
+
+        FloatingActionButton fab = findViewById(R.id.btnchecklistLaporan);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}

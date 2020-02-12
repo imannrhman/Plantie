133cc61f92284bea08e4deae3d45047fbfec8e89
Iqbal_Mubarok <muhammadiqbalmubarok@gmail.com>
2020-02-12 10:34:29 +0700

    fix bug

--- app/src/main/java/com/codates/plantie/adapter/AlarmAdapter.java
+++ app/src/main/java/com/codates/plantie/adapter/AlarmAdapter.java
@@ -4,16 +4,19 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.Person;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.Icon;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.provider.Settings;
 
 import androidx.core.app.NotificationCompat;
+import androidx.core.graphics.drawable.IconCompat;
 import androidx.legacy.content.WakefulBroadcastReceiver;
 
 import com.codates.plantie.R;
@@ -24,6 +27,7 @@ import com.codates.plantie.view.MainActivity;
 import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class AlarmAdapter extends BroadcastReceiver {
+    private String CHANNEL_ID;
 
     @Override
     public void onReceive(Context context, Intent intent){
@@ -38,6 +42,10 @@ public class AlarmAdapter extends BroadcastReceiver {
                 int importance = NotificationManager.IMPORTANCE_HIGH;
 
                 NotificationChannel mChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, importance);
+                mChannel.enableLights(true);
+                mChannel.setLightColor(Color.GREEN);
+                mChannel.setShowBadge(true);
+                mChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
                 notificationManager.createNotificationChannel(mChannel);
             }
 
@@ -49,7 +57,7 @@ public class AlarmAdapter extends BroadcastReceiver {
 
             NotificationCompat.Builder builder = new NotificationCompat.Builder(context,NOTIFICATION_CHANNEL_ID);
             builder.setContentIntent(pendingIntent)
-                    .setSmallIcon(R.mipmap.ic_logo)
+                    .setSmallIcon(R.mipmap.ic_logo_round)
                     .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_alarm_black_24dp))
                     .setTicker("menyiram")
                     .setAutoCancel(true)
@@ -64,4 +72,16 @@ public class AlarmAdapter extends BroadcastReceiver {
 //        }
 
     }
+
+//    private void simple(){
+//        Person person = new Person.Builder()
+//                            .setName("PLANTIE")
+////                            .setIcon(IconCompat.createWithResource(this, R.mipmap.ic_logo_round))
+//                            .setImportant(true)
+//                            .build();
+//
+//        new NotificationCompat.MessagingStyle(person)
+//
+//    }
+
 }
